e3950c6d9ed3f0c40cf310ba8872852f2a17c86e
osirisofgit <github.com.j95fw@passmail.net>
2025-07-13 13:44:18 -0500

    successfully renamed everything, despite microsofts best efforts

---
+++ IntelligentColonists/Source/IntelligentColonists.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace IntelligentColonists
+{
+    [StaticConstructorOnStartup]
+    public static class IntelligentColonists
+    {
+        static IntelligentColonists()
+        {
+            Log.Message("Hello World!");
+        }
+    }
+}
